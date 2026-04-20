67c72c0cf062b52fa238d955497226dea85c0618
Samira Ardani <samira.ardani@noaa.gov>
2026-04-20 20:06:54 +0000

    added these changes just for testing.

--- dev/drivers/scripts/stats/rtofs/jevs_stats_rtofs_argo_grid2obs.sh
+++ dev/drivers/scripts/stats/rtofs/jevs_stats_rtofs_argo_grid2obs.sh
@@ -35,7 +35,7 @@ source $HOMEevs/dev/modulefiles/${COMPONENT}/${COMPONENT}_${STEP}.sh
 evs_ver_2d=$(echo $evs_ver | cut -d'.' -f1-2)
 
 # set up COMIN and COMOUT
-export COMIN=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/${evs_ver_2d}
+export COMIN=/lfs/h2/emc/vpppg/noscrub/emc.vpppg/$NET/${evs_ver_2d}
 export COMOUT=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/${evs_ver_2d}
 export DATAROOT=/lfs/h2/emc/stmp/${USER}/evs_test/$envir/tmp
 
