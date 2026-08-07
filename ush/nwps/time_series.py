19183a5a9ba01c0415fc3fdf828641f0ec7df131
SamiraArdani-NOAA <samira.ardani@noaa.gov>
2026-08-07 19:53:53 +0000

    Updated the code to generate correct image name.

--- ush/nwps/time_series.py
+++ ush/nwps/time_series.py
@@ -1044,8 +1044,7 @@ def plot_time_series(df: pd.DataFrame, logger: logging.Logger,
     domain_string = domain_string.replace(', ','_')
     if str(domain_string).lower() == 'global_0p25':
         domain_string = 'latlon_0p25_glb'
-    save_name = (f'evs.'
-                 + f'{str(models_savename).lower()}.'
+    save_name = (f'evs.nwps.'
                  + f'{str(metric1_name).lower()}.'
                  + f'{str(var_savename).lower()}_{str(level_savename).lower()}_{str(obtype).lower()}.'
                  + f'{str(time_period_savename).lower()}.'
@@ -1054,8 +1053,7 @@ def plot_time_series(df: pd.DataFrame, logger: logging.Logger,
                  + f'{str(domain_string).lower()}_'
                  + f'{str(WFO).lower()}')
     if metric2_name is not None:
-        save_name = (f'evs.'
-                 + f'{str(models_savename).lower()}.'
+        save_name = (f'evs.nwps.'
                  + f'{str(metric1_name).lower()}_{str(metric2_name).lower()}.'
                  + f'{str(var_savename).lower()}_{str(level_savename).lower()}_{str(obtype).lower()}.'
                  + f'{str(time_period_savename).lower()}.'
@@ -1064,8 +1062,7 @@ def plot_time_series(df: pd.DataFrame, logger: logging.Logger,
                  + f'{str(domain_string).lower()}_'
                  + f'{str(WFO).lower()}')
     if thresh and '' not in thresh:
-        save_name = (f'evs.'
-                 + f'{str(models_savename).lower()}.'
+        save_name = (f'evs.nwps.'
                  + f'{str(metric1_name).lower()}_{str(thresholds_save_phrase).lower()}.'
                  + f'{str(var_savename).lower()}_{str(level_savename).lower()}_{str(obtype).lower()}.'
                  + f'{str(time_period_savename).lower()}.'
